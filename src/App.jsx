8f13f3ae6fcd0305bcb36ef3d9117779f16e7d09
sakibhossaindev <sakibhossain.web.dev@gmail.com>
2026-07-16 00:31:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div className='grid-cols-2'>fffff </div>
+    <div className='grid-cols-2'>fffff </div>
+
+    </>
+  )
+}
+
+export default App
