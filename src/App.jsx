deed6bf98e333de1ee443fdb1e9cd8a5d66a3e2d
sakibhossaindev <sakibhossain.web.dev@gmail.com>
2026-08-10 00:33:56 +0600

    login part

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { createRoutesFromElements, createBrowserRouter, Route, RouterProvider } from "react-router-dom";
+import Login from "./Pages/Login";
+import Registration from "./Pages/Registration";
 import Home from "./Pages/Home";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Home />}></Route>
+    <>
+      <Route path="/" element={<Login />}></Route>
+      <Route path="/Registration" element={<Registration />}></Route>
+      <Route path="/Home" element={<Home />}></Route></>
   )
 );
 
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import TextField from '@mui/material/TextField';
+import { FcGoogle } from "react-icons/fc";
+import Button from '@mui/material/Button';
 
-const login = () => {
-    return (
-        <>
-        
 
-       
-        </>
-    )
+const Login = () => {
+  return (
+    <>
+      <div className='bg-amber-300 w-2xs m-auto text-center'>
+        <h1>Login to your account!</h1>
+        <FcGoogle /><p>Login with Google</p>
+        <TextField sx={{}} id="outlined-basic" label="Email" variant="outlined" type='email' />
+        <TextField id="outlined-basic" label="password" variant="outlined" type='password' />
+        <Button variant="contained">Contained</Button>
+        <p>Don’t have an account ?  Sign up</p>
+      </div>
+
+    </>
+  )
 }
 
-export default login
+export default Login
